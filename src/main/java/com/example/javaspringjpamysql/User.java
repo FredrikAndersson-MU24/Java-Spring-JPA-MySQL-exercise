832dcba15de4eb113bb3dcc2dd8acc17ee43b793
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-03-05 08:22:54 +0100

    Inital commit

---
+++ src/main/java/com/example/javaspringjpamysql/User.java
@@ -0,0 +1,4 @@
+package com.example.javaspringjpamysql;
+
+public class User {
+}
---
+++ src/test/java/com/example/javaspringjpamysql/JavaSpringJpaMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringjpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringJpaMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
